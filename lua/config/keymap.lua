6b08c08109db93260304c29e8f62e7863f5c6513
Divakar-2508 <divakaraugust@gmail.com>
2025-07-02 19:14:17 +0530

    changed lint

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -9,29 +9,29 @@ vim.keymap.set("n", "<S-Tab>", ":bprevious<CR>", { silent = true })
 vim.keymap.set("n", "<leader>x", ":bd<CR>", { silent = true })
 -- Line Comment
 vim.keymap.set(
-	"n",
-	"<leader>/l",
-	"<cmd>lua require('Comment.api').toggle.linewise.current()<CR>",
-	{ desc = "Toggle Line Comment" }
+    "n",
+    "<leader>/l",
+    "<cmd>lua require('Comment.api').toggle.linewise.current()<CR>",
+    { desc = "Toggle Line Comment" }
 )
 vim.keymap.set(
-	"v",
-	"<leader>/l",
-	"<esc><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>",
-	{ desc = "Toggle Line Comment" }
+    "v",
+    "<leader>/l",
+    "<esc><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>",
+    { desc = "Toggle Line Comment" }
 )
 -- Block Comment
 vim.keymap.set(
-	"n",
-	"<leader>/b",
-	"<cmd>lua require('Comment.api').toggle.blockwise.current()<CR>",
-	{ desc = "Toggle Block Comment" }
+    "n",
+    "<leader>/b",
+    "<cmd>lua require('Comment.api').toggle.blockwise.current()<CR>",
+    { desc = "Toggle Block Comment" }
 )
 vim.keymap.set(
-	"v",
-	"<leader>/b",
-	"<esc><cmd>lua require('Comment.api').toggle.blockwise(vim.fn.visualmode())<CR>",
-	{ desc = "Toggle Block Comment" }
+    "v",
+    "<leader>/b",
+    "<esc><cmd>lua require('Comment.api').toggle.blockwise(vim.fn.visualmode())<CR>",
+    { desc = "Toggle Block Comment" }
 )
 
 -- Window Height
@@ -43,115 +43,121 @@ vim.keymap.set("n", "<M-Left>", ":vertical resize -2<CR>", { desc = "Decrease wi
 -- inlay_hints
 local inlay_hints = false
 local function toggle_inlay_hints()
-	local bufnr = vim.api.nvim_get_current_buf()
+    local bufnr = vim.api.nvim_get_current_buf()
 
-	-- Ensure LSP is attached
-	local clients = vim.lsp.buf_get_clients(bufnr)
-	if #clients == 0 then
-		print("No LSP server attached to the current buffer")
-		return
-	end
+    -- Ensure LSP is attached
+    local clients = vim.lsp.buf_get_clients(bufnr)
+    if #clients == 0 then
+        print("No LSP server attached to the current buffer")
+        return
+    end
 
-	-- Check if inlay hint provider is available for the current LSP client
-	local capabilities = clients[1].server_capabilities
-	if capabilities.inlayHintProvider then
-		-- Toggle inlay hint
-		inlay_hints = not inlay_hints
-		vim.lsp.inlay_hint.enable(inlay_hints)
-		if inlay_hints then
-			print("Inlay Hints enabled")
-		else
-			print("Inlay Hints Disabled")
-		end
-	else
-		print("Inlay hints are not supported by the current LSP server")
-	end
+    -- Check if inlay hint provider is available for the current LSP client
+    local capabilities = clients[1].server_capabilities
+    if capabilities.inlayHintProvider then
+        -- Toggle inlay hint
+        inlay_hints = not inlay_hints
+        vim.lsp.inlay_hint.enable(inlay_hints)
+        if inlay_hints then
+            print("Inlay Hints enabled")
+        else
+            print("Inlay Hints Disabled")
+        end
+    else
+        print("Inlay hints are not supported by the current LSP server")
+    end
 end
 
 vim.keymap.set("n", ".h", function()
-	toggle_inlay_hints()
+    toggle_inlay_hints()
 end, { noremap = true, silent = true })
 
 vim.api.nvim_create_autocmd("LspAttach", {
-	callback = function(event)
-		local bufnr = event.buf
-		local clients = vim.lsp.buf_get_clients(bufnr)
-		if #clients == 0 then
-			return
-		end
+    callback = function(event)
+        local bufnr = event.buf
+        local clients = vim.lsp.buf_get_clients(bufnr)
+        if #clients == 0 then
+            return
+        end
 
-		local capabilities = clients[1].server_capabilities
-		if capabilities.inlayHintProvider then
-			print("Inlay hints are supported. Use .h to toggle.")
-		else
-			print("Inlay hints are not supported by the attached LSP server.")
-		end
-	end,
+        local capabilities = clients[1].server_capabilities
+        if capabilities.inlayHintProvider then
+            print("Inlay hints are supported. Use .h to toggle.")
+        else
+            print("Inlay hints are not supported by the attached LSP server.")
+        end
+    end,
 })
 
 function lsp_bindings()
-	local nmap = function(keys, func, desc)
-		vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
-	end
+    local nmap = function(keys, func, desc)
+        vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
+    end
 
-	-- LSP-related keybindings
-	nmap("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
-	nmap("gr", vim.lsp.buf.references, "[G]oto [R]eferences")
-	nmap("gi", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
-	nmap("K", vim.lsp.buf.hover, "Hover Documentation")
-	nmap("<C-k>", vim.lsp.buf.signature_help, "Signature Help")
-	nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[N]ame")
-	nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
+    -- LSP-related keybindings
+    nmap("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
+    nmap("gr", vim.lsp.buf.references, "[G]oto [R]eferences")
+    nmap("gi", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
+    nmap("K", vim.lsp.buf.hover, "Hover Documentation")
+    nmap("<C-k>", vim.lsp.buf.signature_help, "Signature Help")
+    nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[N]ame")
+    nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
 
-	-- Diagnostics keybindings
-	nmap("[d", vim.diagnostic.goto_prev, "Go to Previous Diagnostic")
-	nmap("]d", vim.diagnostic.goto_next, "Go to Next Diagnostic")
-	nmap("<leader>e", vim.diagnostic.open_float, "[E]rror Details")
-	nmap("<leader>q", vim.diagnostic.setloclist, "[Q]uickfix Diagnostics List")
+    -- Diagnostics keybindings
+    nmap("[d", vim.diagnostic.goto_prev, "Go to Previous Diagnostic")
+    nmap("]d", vim.diagnostic.goto_next, "Go to Next Diagnostic")
+    nmap("<leader>e", vim.diagnostic.open_float, "[E]rror Details")
+    nmap("<leader>q", vim.diagnostic.setloclist, "[Q]uickfix Diagnostics List")
 
-	-- Format the buffer
-	nmap("<leader>gf", vim.lsp.buf.format, "[F]ormat Document")
+    -- Format the buffer
+    nmap("<leader>gf", vim.lsp.buf.format, "[F]ormat Document")
 end
 
 vim.api.nvim_create_autocmd("LspAttach", { callback = lsp_bindings })
 
 local function compile_and_run()
-	local file_path = vim.fn.expand("%:p:h") -- Get the directory of the current file
-	local file_name_ext, file_type = vim.fn.expand("%:t"), vim.bo.filetype
-	local file_name = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ":t:r")
+    local file_path = vim.fn.expand("%:p:h") -- Get the directory of the current file
+    local file_name_ext, file_type = vim.fn.expand("%:t"), vim.bo.filetype
+    local file_name = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ":t:r")
 
-	local term_bufnr = nil
-	for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
-		if vim.bo[bufnr].buftype == "terminal" then
-			term_bufnr = bufnr
-			break
-		end
-	end
+    local term_bufnr = nil
+    for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
+        if vim.bo[bufnr].buftype == "terminal" then
+            term_bufnr = bufnr
+            break
+        end
+    end
 
-	if not term_bufnr then
-		vim.cmd("term")
-		term_bufnr = vim.api.nvim_get_current_buf()
-	else
-		vim.api.nvim_set_current_buf(term_bufnr)
-	end
+    if not term_bufnr then
+        vim.cmd("term")
+        term_bufnr = vim.api.nvim_get_current_buf()
+    else
+        vim.api.nvim_set_current_buf(term_bufnr)
+    end
 
-	local command = ({
-		cpp = string.format(
-			"cd %s; clang++ --std=c++23 %s -o %s; ./%s",
-			file_path,
-			file_name_ext,
-			file_name,
-			file_name
-		),
-		java = string.format("cd %s; javac %s; java %s", file_path, file_name_ext, file_name),
-		c = string.format("cd %s; clang %s -o %s; ./%s", file_path, file_name_ext, file_name, file_name),
-	})[file_type]
+    local command = ({
+        cpp = string.format(
+            "cd %s; clang++ --std=c++23 %s -o %s; ./%s",
+            file_path,
+            file_name_ext,
+            file_name,
+            file_name
+        ),
+        java = string.format("cd %s; javac %s; java %s", file_path, file_name_ext, file_name),
+        c = string.format("cd %s; clang %s -o %s; ./%s", file_path, file_name_ext, file_name, file_name),
+    })[file_type]
 
-	if command then
-		vim.fn.chansend(vim.api.nvim_buf_get_var(term_bufnr, "terminal_job_id"), command .. "\n")
-	else
-		print("Unsupported file type.")
-	end
+    if command then
+        vim.fn.chansend(vim.api.nvim_buf_get_var(term_bufnr, "terminal_job_id"), command .. "\n")
+    else
+        print("Unsupported file type.")
+    end
 end
 
 vim.keymap.set("n", "<leader>cr", compile_and_run, { desc = "Compile & Run" })
+
+vim.keymap.set('n', '<leader>cp', function()
+    local dir = vim.fn.expand('%:p:h')
+    vim.fn.setreg('+', dir)
+    print('Copied directory: ' .. dir)
+end, { desc = 'Copy file directory to clipboard' })
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,192 +1,194 @@
 return {
-	{
-		"loctvl842/monokai-pro.nvim",
-		config = function()
-			vim.cmd("colorscheme monokai-pro-spectrum")
-		end,
-	},
-	{
-		"windwp/nvim-autopairs",
-		lazy = true,
-		event = "InsertEnter",
-		config = true,
-		-- use opts = {} for passing setup options
-		-- this is equivalent to setup({}) function
-	},
-	{
-		"akinsho/bufferline.nvim",
-		config = function()
-			require("bufferline").setup()
-		end,
-	},
-	{
-		"stevearc/conform.nvim",
-		opts = {},
-		config = function()
-			require("conform").setup({
-				formatters_by_ft = {
-					lua = { "stylua" },
-					rust = { "rustfmt", lsp_format = "fallback" },
-					html = { "prettier" },
-					css = { "prettier" },
-					csharp = { "csharpier" },
-					python = {
-						"ruff_fix",
-						"ruff_format",
-						"ruff_organize_imports",
-					},
-					asm = { "asmfmt" },
-				},
-				format_on_save = {
-					timeout_ms = 500,
-					lsp_format = "fallback",
-				},
-			})
-		end,
-	},
-	{
-		"ibhagwan/fzf-lua",
-		dependencies = { { "echasnovski/mini.icons", opts = {} } }, -- Optional for icons
-		config = function()
-			require("fzf-lua").setup({
-				winopts = {
-					height = 0.85, -- window height
-					width = 0.80, -- window width
-					row = 0.30, -- window row position
-					col = 0.50, -- window col position
-					border = "rounded", -- border style
-				},
-				files = {
-					prompt = "Files❯ ",
-					cmd = "fd --type f --hidden --follow --exclude .git", -- Use fd for file search
-					git_icons = true, -- Show git icons for files
-					file_icons = true, -- Show file type icons
-				},
-				grep = {
-					rg_opts = "--column --line-number --no-heading --color=always --smart-case",
-					exact = false,
-					case_mode = "smart_case",
-					-- prompt = "Rg❯ ",
-					-- cmd = "rg --vimgrep" -- Use ripgrep for searching
-				},
-				previewers = {
-					bat = {
-						cmd = "bat",
-						args = "--style=numbers,changes --color always",
-						theme = "OneHalfDark", -- Set the theme for bat
-						-- theme = "monokai-pro-spectrum",
-						config = nil, -- Set this to a custom bat config file if needed
-					},
-				},
-			})
-			local fzf = require("fzf-lua")
-			vim.keymap.set("n", "<leader>f", fzf.files, { desc = "Fuzzy search files" })
-			vim.keymap.set("n", "<leader>s", fzf.lsp_workspace_symbols, { desc = "Search workspace symbols" })
-			vim.keymap.set("n", "<leader>rg", fzf.grep, { desc = "Search with ripgrep" })
-			vim.keymap.set("n", "<leader>b", fzf.buffers, { desc = "Switch buffers" })
-			vim.keymap.set("n", "<leader>l", fzf.live_grep, { desc = "Live grep search" })
-			vim.keymap.set("n", "<leader>t", fzf.tags, { desc = "Search tags" })
-		end,
-	},
-	{
-		"nvim-treesitter/nvim-treesitter",
-		run = ":TSUpdate",
-		event = "BufReadPre",
-		config = function()
-			-- Specify the compiler for Windows
-			require("nvim-treesitter.install").compilers = { "clang" }
+    {
+        "loctvl842/monokai-pro.nvim",
+        config = function()
+            vim.cmd("colorscheme monokai-pro-spectrum")
+        end,
+    },
+    {
+        "windwp/nvim-autopairs",
+        lazy = true,
+        event = "InsertEnter",
+        config = true,
+        -- use opts = {} for passing setup options
+        -- this is equivalent to setup({}) function
+    },
+    {
+        "akinsho/bufferline.nvim",
+        config = function()
+            require("bufferline").setup()
+        end,
+    },
+    {
+        "stevearc/conform.nvim",
+        opts = {},
+        config = function()
+            require("conform").setup({
+                formatters_by_ft = {
+                    lua = { "stylua" },
+                    rust = { "rustfmt", lsp_format = "fallback" },
+                    html = { "prettier" },
+                    css = { "prettier" },
+                    csharp = { "csharpier" },
+                    python = {
+                        "ruff_fix",
+                        "ruff_format",
+                        "ruff_organize_imports",
+                    },
+                    asm = { "asmfmt" },
+                },
+                format_on_save = {
+                    timeout_ms = 500,
+                    lsp_format = "fallback",
+                },
+            })
+        end,
+    },
+    {
+        "ibhagwan/fzf-lua",
+        dependencies = { { "echasnovski/mini.icons", opts = {} } }, -- Optional for icons
+        config = function()
+            require("fzf-lua").setup({
+                winopts = {
+                    height = 0.85,      -- window height
+                    width = 0.80,       -- window width
+                    row = 0.30,         -- window row position
+                    col = 0.50,         -- window col position
+                    border = "rounded", -- border style
+                },
+                files = {
+                    prompt = "Files❯ ",
+                    cmd = "fd --type f --hidden --follow --exclude .git", -- Use fd for file search
+                    git_icons = true,                                     -- Show git icons for files
+                    file_icons = true,                                    -- Show file type icons
+                },
+                grep = {
+                    rg_opts = "--column --line-number --no-heading --color=always --smart-case",
+                    exact = false,
+                    case_mode = "smart_case",
+                    -- prompt = "Rg❯ ",
+                    -- cmd = "rg --vimgrep" -- Use ripgrep for searching
+                },
+                previewers = {
+                    bat = {
+                        cmd = "bat",
+                        args = "--style=numbers,changes --color always",
+                        theme = "OneHalfDark", -- Set the theme for bat
+                        -- theme = "monokai-pro-spectrum",
+                        config = nil,          -- Set this to a custom bat config file if needed
+                    },
+                },
+            })
+            local fzf = require("fzf-lua")
+            vim.keymap.set("n", "<leader>f", fzf.files, { desc = "Fuzzy search files" })
+            vim.keymap.set("n", "<leader>s", fzf.lsp_workspace_symbols, { desc = "Search workspace symbols" })
+            vim.keymap.set("n", "<leader>rg", fzf.grep, { desc = "Search with ripgrep" })
+            vim.keymap.set("n", "<leader>b", fzf.buffers, { desc = "Switch buffers" })
+            vim.keymap.set("n", "<leader>l", fzf.live_grep, { desc = "Live grep search" })
+            vim.keymap.set("n", "<leader>t", fzf.tags, { desc = "Search tags" })
+        end,
+    },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        run = ":TSUpdate",
+        event = "BufReadPre",
+        config = function()
+            -- Specify the compiler for Windows
+            -- require("nvim-treesitter.install").compilers = { "gcc" }
 
-			-- Treesitter setup
-			require("nvim-treesitter.configs").setup({
-				ensure_installed = { "rust", "python", "lua", "cpp", "java", "c" }, -- Add any languages you need
-				highlight = {
-					enable = true, -- Enable syntax highlighting
-					disable = {}, -- Disable highlighting for certain languages (optional)
-				},
-				incremental_selection = {
-					enable = true, -- Enable incremental selection
-					keymaps = {
-						init_selection = "gnn",
-						node_incremental = "grn",
-						scope_incremental = "grc",
-						node_decremental = "grm",
-					},
-				},
-			})
-		end,
-	},
-	{
-		"williamboman/mason.nvim",
-		lazy = true,
-		cmd = "Mason",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		dependencies = { "williamboman/mason.nvim", "neovim/nvim-lspconfig" },
-		lazy = true,
-	},
-	{
-		"mrcjkb/rustaceanvim",
-		version = "^5", -- Recommended
-		lazy = false, -- This plugin is already lazy
-	},
-	{
-		"numToStr/Comment.nvim",
-		lazy = true,
-		config = function()
-			require("Comment").setup({})
-		end,
-	},
-	{
-		"mfussenegger/nvim-lint",
-		lazy = true,
-		event = "BufWritePost",
-		config = function()
-			local lint = require("lint")
+            -- Treesitter setup
+            require("nvim-treesitter.configs").setup({
+                ensure_installed = { "rust", "python", "lua", "cpp", "java", "c" }, -- Add any languages you need
+                highlight = {
+                    enable = true,                                                  -- Enable syntax highlighting
+                    disable = {},                                                   -- Disable highlighting for certain languages (optional)
+                },
+                incremental_selection = {
+                    enable = true, -- Enable incremental selection
+                    keymaps = {
+                        init_selection = "gnn",
+                        node_incremental = "grn",
+                        scope_incremental = "grc",
+                        node_decremental = "grm",
+                    },
+                },
+            })
+        end,
+    },
+    {
+        "williamboman/mason.nvim",
+        lazy = true,
+        cmd = "Mason",
+        config = function()
+            require("mason").setup()
+        end,
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        dependencies = { "williamboman/mason.nvim", "neovim/nvim-lspconfig" },
+        lazy = true,
+        config = function()
+            require("mason-lspconfig").setup({
+                handlers = {}
+            })
+        end
+    },
+    {
+        "mrcjkb/rustaceanvim",
+        version = "^5", -- Recommended
+        lazy = false,   -- This plugin is already lazy
+    },
+    {
+        "numToStr/Comment.nvim",
+        lazy = true,
+        config = function()
+            require("Comment").setup({})
+        end,
+    },
+    {
+        "mfussenegger/nvim-lint",
+        lazy = true,
+        event = "BufWritePost",
+        config = function()
+            local lint = require("lint")
 
-			-- Set up linters for different file types
-			lint.linters_by_ft = {
-				rust = { "clippy" },
-				-- cpp = { "clangtidy" },
-				python = { "ruff" },
-			}
+            -- Set up linters for different file types
+            lint.linters_by_ft = {
+                rust = { "clippy" },
+                -- cpp = { "clangtidy" },
+                javascript = { "eslint_d" },
+                python = { "ruff" },
+            }
 
-			-- Create a unique augroup
-			local lint_augroup = vim.api.nvim_create_augroup("Linting", { clear = true })
+            local lint_augroup = vim.api.nvim_create_augroup("Linting", { clear = true })
 
-			-- Auto-trigger linting on BufEnter, BufWritePost, InsertLeave
-			vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
-				group = lint_augroup,
-				callback = function()
-					lint.try_lint()
-				end,
-			})
+            vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+                group = lint_augroup,
+                callback = function()
+                    lint.try_lint()
+                end,
+            })
 
-			-- Debug: Show if linting is actually running
-			vim.api.nvim_create_user_command("LintNow", function()
-				print("Running Linter...")
-				lint.try_lint()
-				print("Lint finished...")
-			end, {})
+            vim.api.nvim_create_user_command("LintNow", function()
+                print("Running Linter...")
+                lint.try_lint()
+                print("Lint finished...")
+            end, {})
 
-			-- Run linting on startup (optional)
-			vim.defer_fn(function()
-				lint.try_lint()
-			end, 100)
-		end,
-	},
-	{
-		"MeanderingProgrammer/render-markdown.nvim",
-		lazy = true,
-		ft = "markdown",
-		dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.icons" }, -- if you use the mini.nvim suite
-		-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
-		-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
-		---@module 'render-markdown'
-		---@type render.md.UserConfig
-		opts = {},
-	},
+            vim.defer_fn(function()
+                lint.try_lint()
+            end, 100)
+        end,
+    },
+    {
+        "MeanderingProgrammer/render-markdown.nvim",
+        lazy = true,
+        ft = "markdown",
+        dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.icons" }, -- if you use the mini.nvim suite
+        -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+        -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+        ---@module 'render-markdown'
+        ---@type render.md.UserConfig
+        opts = {},
+    },
 }
